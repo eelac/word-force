f0ffb4c85738f0db8b633b1312082f83bd97168d
Jonathan Soukaseume <sksmejn@gmail.com>
2021-01-27 16:51:08 -0800

    simplified ALL functions and made them much more modular than previous. Everything works the same as it did before. Created a clear field function to just run on any target field

--- main.js
+++ main.js
@@ -2,22 +2,22 @@
 console.log("hello world");
 
 // Click or push enter to work
-$("#search-button").click(function(e){
+$("#search-button").click(function(e) {
     e.preventDefault();
     var userVALUE = $("#user-input").val();
 
     // catches empty field
-    if(userVALUE == null || userVALUE == ""){
+    if(userVALUE == null || userVALUE == "") {
         alert("Please enter a word!");
     } else {
         // Testing console
-        console.log(userVALUE.trim());
+        console.log(userVALUE);
         searchWord(userVALUE.trim());
-        $("#user-input").val("");
+        clearField("#user-input");
     }
 })
 
-function searchWord(userVALUE){
+function searchWord(userVALUE) {
     // Dictionary items
     var dictKEY = "?key=e88ef112-883a-4f54-975c-e1af4ff0d8c3";
     var dictURL = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" 
@@ -27,11 +27,10 @@ function searchWord(userVALUE){
     $.ajax({
         type: "GET",
         url: dictURL
-    }).done(function(data){
+    }).done(function(data) {
         //Testing dictionary link
         console.log("Full DICTIONARY response:");
         console.log(data);
-        // Not all words have etymology!
         // Catches if random nonesense is input
         if(data.length === 0) {
             alert("Please enter a valid entry.");
@@ -41,7 +40,7 @@ function searchWord(userVALUE){
         } else {
             var etym = uncurl(data[0].et[0][1]);
             // Clears Information field
-            $(".post-here").text("");
+            clearField(".post-here");
             // Creates title with userValue
             postAppend("<h1 class='value-text'>" + userVALUE.toLowerCase().trim() + "</h1>", ".post-here");
             // Creates Definition
@@ -54,19 +53,19 @@ function searchWord(userVALUE){
             searchThesaurus(userVALUE.toLowerCase().trim());
 
             // Clears history field
-            $(".history-title-here").text("");
+            clearField(".history-title-here");
             // Creates Last 5 Search History Title Text
-            $(".history-title-here").append("<h3 class='search-history-text'><u>Previous Searches:</u></h3>");
+            postAppend("<h3 class='search-history-text'><u>Previous Searches:</u></h3>", ".history-title-here");
             // Appends userValue into history 
             historyAppend(userVALUE.toLowerCase().trim());
         }
-    }).fail(function(){
+    }).fail(function() {
         console.log("failed");
     })
 }
 
 // Thesaurus function
-function searchThesaurus(userVALUE){
+function searchThesaurus(userVALUE) {
     // Thesaurus items
     var thesKEY = "?key=a6fccfe6-b8af-43f4-9d56-746368f04ea1";
     var thesURL = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/" 
@@ -81,11 +80,12 @@ function searchThesaurus(userVALUE){
     }).done(function(data) {
         console.log(data);
         if(!data[0].meta) {
-            $(".synonym-here").text("");
+            // Clear synonym field
+            clearField(".synonym-here");
             postAppend("<strong>Synonyms:</strong> No synonyms available.", ".synonym-here");
         } else {
             // Clears synonym field
-            $(".synonym-here").text("");
+            clearField(".synonym-here");
             var syns = data[0].meta.syns[0];
             postAppend("<strong>Synonyms:</strong> " + syns.join(", "), ".synonym-here");
         }
@@ -94,54 +94,44 @@ function searchThesaurus(userVALUE){
     })
 }
 
-// changes opacity of the cards
-$(".card-show").css("opacity", "0.6").css("transition", "0.5s");
-// on mouseover changes darkens opacity
-$(".card-show").on("mouseover", function() {
-    $(this).css("opacity", "1");
-})
-// on mouseout, changes opacity back to  light
-$(".card-show").on("mouseout", function() {
-    $(this).css("opacity", "0.6");
-})
+// Fades out cards by default and fades in on hover
+fadingIn(".card-show", "0.6", "1", "0.5s");
 
 // Click cards start fuctions
-$(".card-show").click(function(){
+$(".card-show").click(function() {
     var dataValue = $(this).data("value");
-    if(dataValue === 1){
+    if(dataValue === 1) {
         // old norse / dutch /scandinavian
         var norse = [
             "anger", "cake", "viking", "reindeer", "outlaw", "raft", "bumpkin", "awkward", "bag", "dirt", 
             "die", "caboose", "bait", "egg", "coleslaw", "cashier", "dapper", "furlough", "iceberg", "luck"
         ];
-        searchWord(looper(norse));
-    } else if(dataValue === 2){
+        searchWord(randomLoop(norse));
+    } else if(dataValue === 2) {
         // chinese / japanese
         var chinese = [
             "brainwash", "ketchup", "typhoon", "chowchow", "kumquat", "ramen", "tycoon", "wok", "tofu", "tea", 
-            "rickshaw", "karaoke", "emoji", "origami", "sayonara", "wonton", "zen", "shiatsu"
+            "rickshaw", "karaoke", "emoji", "origami", "sayonara", "wonton", "zen", "shiatsu", "sushi"
         ];
-        searchWord(looper(chinese));
+        searchWord(randomLoop(chinese));
     } else {
         // spanish
         var spanish = [
             "banana", "cockroach", "crimson", "mustang", "embargo", "guacamole", "guerrilla", "hurricane", "macho", "mosquito", 
             "patio", "alligator", "tango", "matador", "cargo", "renegade", "vanilla", "bonanza", "tomato", "torque"
         ];
-        searchWord(looper(spanish));
+        searchWord(randomLoop(spanish));
     }
 })
 
 //Appends a list of history
 function historyAppend(text) {
-    var li = $("<li>");
-    var ul = $(".history-here");
-    ul.append(li.append(text));
+    $(".history-here").append($("<li>").append(text));
 
     // Added max history list limit 5
     var maxHistory = 5;
 
-    $(".history-here").each(function(){
+    $(".history-here").each(function() {
         $(this).find("li").each(function(index){
             if(index >= maxHistory){
                 $(".history-here > li:first-child").remove();
@@ -150,23 +140,35 @@ function historyAppend(text) {
     })
 }
 
-// Appends the required list of the word typed onto the page, above the history
+// Fade out an object and fade them back in on hover
+function fadingIn(object, opacityStart, opacityEnd, time) {
+    // changes opacity of the cards
+    $(object).css("opacity", opacityStart).css("transition", time);
+    // on mouseover changes darkens opacity
+    $(object).on("mouseover", function() {
+        $(this).css("opacity", opacityEnd);
+    })
+    // on mouseout, changes opacity back to  light
+    $(object).on("mouseout", function() {
+        $(this).css("opacity", opacityStart);
+    })
+}
+
+// Appends the required list of the word typed onto the page with provided target container
 function postAppend(text, targetContainer) {
-    var div = $("<div>");
-    var divContainer = $(targetContainer);
-    divContainer.append(div.append(text));
+    $(targetContainer).append($("<div>").append(text));
 }
 
 // Removes curls around API data's {it}...{/it}
 function uncurl(text) {
-    var uncurled = text
-        .replaceAll("{it}", "<i>")
-        .replaceAll("{/it}", "</i>");
-    return uncurled;
+    return text.replaceAll("{it}", "<i>").replaceAll("{/it}", "</i>");
 }
 
 // Loops through certain arrays in random order
-function looper(array){
-    var random = Math.floor(Math.random() * array.length);
-    return array[random];
+function randomLoop(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function clearField(object) {
+    $(object).text("").val("");
 }
